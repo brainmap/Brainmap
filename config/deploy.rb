38fe7ef3d9176bfb9e99d487317b391b0396e2c7
Kristopher Kosmatka <kjklocal@kearney.medicine.wisc.edu>
2009-07-16 12:18:13 -0500

    Fixed some bugs in capistrano deployment.  We now get a fresh new copy of the previous production db at each deployment.  Hence we can iterate back through releases, complete with their db.  Meanwhile we preserve db content across each release.  Cool.  DB migrations should just work with cap deploy:migrate.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,8 +9,6 @@ role :db,  host_server, :primary => true
 
 set :user, "deployer"
 set :deploy_to, "/var/www/html/#{application}"
-# set :deploy_via, :remote_cache
-set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:brainmap/Brainmap.git"
@@ -19,26 +17,17 @@ set :branch, "master"
 namespace :deploy do
   # task :update_code
   
+  # task :symlink
+  
   desc "Symlink shared configs and folders on each release."
   task :symlink_shared do
     run "cp #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
-    
     run "rm -rf #{release_path}/config/database.yml"
     run "rm -rf #{release_path}/images/*"
-    run "rm -rf #{release_path}/system/*"
-    run "rm -rf #{release_path}/db/production.sqlite3"
     
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/images #{release_path}/public/images"
-    run "ln -nfs #{shared_path}/system #{release_path}/public/system"
     run "ln -nfs #{release_path}/db/production.sqlite3 #{shared_path}/db/production.sqlite3"
-  end
-  
-  # task :symlink
-  
-  desc "Set ownerships of release for apache"
-  task :update_ownerships do
-    run "chown -R apache:apache #{release_path}"
   end
   
   desc "Tell Passenger to restart the app."
@@ -49,7 +38,4 @@ namespace :deploy do
   # task :migrate
 end
 
-
-
-after 'deploy:update_code', 'deploy:symlink_shared'
-# after 'deploy:symlink', 'deploy:update_ownerships'
+after 'deploy:symlink', 'deploy:symlink_shared'
