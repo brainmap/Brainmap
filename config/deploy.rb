a11a4ed96ee74501683f8ff112904bf2dd29aed5
Kristopher Kosmatka <kjklocal@kearney.medicine.wisc.edu>
2009-07-22 16:07:43 -0500

    Added management of admin users. Cap deployment changed to link shared assets across new deployments.

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,13 +21,15 @@ namespace :deploy do
   
   desc "Symlink shared configs and folders on each release."
   task :symlink_shared do
+    run "rm -rf #{release_path}/public/assets"
     run "cp #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
     run "rm -rf #{release_path}/config/database.yml"
     #run "rm -rf #{release_path}/images/*"
     
+    run "ln -nfs #{shared_path}/assets #{release_path}/public/assets"
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    #run "ln -nfs #{shared_path}/images #{release_path}/public/images"
     run "ln -nfs #{release_path}/db/production.sqlite3 #{shared_path}/db/production.sqlite3"
+    #run "ln -nfs #{shared_path}/images #{release_path}/public/images"
   end
   
   desc "Tell Passenger to restart the app."
