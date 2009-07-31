3117b5edce029505475dc134b3bcae6d83fcbe81
Kristopher Kosmatka <kosmatka@Oskar6.local>
2009-07-31 17:27:39 -0500

    Improved styling and layout for opportunites resources and news.  Note resources index page now pulls its content from a root level page with permalink 'welcome'.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,7 +20,8 @@ class EventsController < ApplicationController
   # GET /events/1.xml
   def show
     @event = Event.find(params[:id])
-
+    @recent_events = Event.all.first(5)
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @event }
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,9 +1,14 @@
 module PagesHelper
-  def children_list(page)
-    output = "<li>#{link_to page.title, page}</li>"
+  def children_list(page, current_page = nil)
+    output = ""
+    if page == current_page
+      output += "<li style='list-style-image:url(../images/objectives-bullet.jpg);'>#{page.title}</li>"
+    else
+      output = "<li>#{link_to page.title, page}</li>"
+    end
     output += "<ul>"
     page.children.each do |child|
-      output += children_list(child)
+      output += children_list(child, current_page)
     end
     output += "</ul>"
     return output
