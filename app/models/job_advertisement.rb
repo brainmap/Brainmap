2cde3b9d0d9114447bf18e0b9cdfe3eef7d4e29a
Kristopher Kosmatka <kjklocal@kearney.medicine.wisc.edu>
2009-08-13 16:27:15 -0500

    Job Ad pvl validation was set to allow_nil => true.  Changed to allow_blank =>  true.

--- app/models/job_advertisement.rb
+++ app/models/job_advertisement.rb
@@ -2,5 +2,5 @@ class JobAdvertisement < ActiveRecord::Base
   belongs_to :responder, :class_name => 'Person', :foreign_key => 'responder_id'
   
   validates_presence_of :title, :body, :responder, :opp_type
-  validates_uniqueness_of :pvl, :allow_nil => true
+  validates_uniqueness_of :pvl, :allow_blank => true
 end
