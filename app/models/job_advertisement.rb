e35a92e3d43c3789027b9274367f48c65f19895f
Kris Kos <kris@jimbo.medicine.wisc.edu>
2009-09-08 13:57:03 -0500

    Allowing opporunity to have no responder.

--- app/models/job_advertisement.rb
+++ app/models/job_advertisement.rb
@@ -1,6 +1,6 @@
 class JobAdvertisement < ActiveRecord::Base
   belongs_to :responder, :class_name => 'Person', :foreign_key => 'responder_id'
   
-  validates_presence_of :title, :body, :responder, :opp_type
+  validates_presence_of :title, :body, :opp_type
   validates_uniqueness_of :pvl, :allow_blank => true
 end
